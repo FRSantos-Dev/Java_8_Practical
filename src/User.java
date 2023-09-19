09f0ceb9f7922bd17eae5d8c796d65687785c214
Felipe Rocha dos Santos <99603179+FRSantos-Dev@users.noreply.github.com>
2023-09-19 20:28:37 -0300

    User is the first class and just to have its constructor

---
+++ src/User.java
@@ -0,0 +1,36 @@
+package src;
+    class User {
+    private String name;
+    private int score;
+    private boolean moderator;
+
+    public User(String name, int score) {
+        this.name = name;
+        this.score = score;
+        this.moderator = moderator;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public boolean isModerator() {
+        return moderator;
+    }
+
+    public void setModerator(boolean moderator) {
+        this.moderator = moderator;
+    }
+}
